35b723e2dd9524128d814172dcb5fcb5840db266
Mark Hoogkamer <mark.hoogkamer@gmail.com>
2021-03-29 12:17:04 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,18 +8,18 @@ namespace helloWorld
         {
 
 
-            Console.WriteLine("Today it's " + DateTime.Now.ToString() + "May I ask for your name?");
+            Console.WriteLine("Today it's " + DateTime.Now.ToString() + " May I ask for your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Hi, " + name + " nice to meet you. Are you male or female?");
             string gender = Console.ReadLine();
 
             if (gender == "Male")
             {
-                Console.WriteLine("You've got some balls man, being " + gender);
+                Console.WriteLine("Must be great, being " + gender);
             }
             else
             {
-                Console.WriteLine("Nice tits, the perks of being " + gender);
+                Console.WriteLine("Ah, the perks of being " + gender);
             }
 
             Console.WriteLine("Nice to see that you are a " + gender);
